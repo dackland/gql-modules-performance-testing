09f7768607bf703c7e3e247ef074442d4772aa08
Daniel Ackland <dan.ackland@gmail.com>
2021-06-01 21:11:00 -0400

    Cleaning up

---
+++ v1/apollo/movie.ts
@@ -0,0 +1,24 @@
+import { createModule, gql } from "graphql-modules";
+const data = require("../../movies/data.json");
+
+export const MovieModule = createModule({
+  id: "movies",
+  dirname: __dirname,
+  typeDefs: gql`
+    type Movie {
+      title: String!
+      year: Int!
+      cast: [String!]!
+      genres: [String!]!
+    }
+
+    type Query {
+      movies: [Movie!]!
+    }
+  `,
+  resolvers: {
+    Query: {
+      movies: () => data,
+    },
+  },
+});
